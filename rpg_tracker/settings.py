dbbafbfc0b1b24b6d7d7d38cdabf5df6bd9875e6
Lucas Westphal <westphallucas1@outlook.com>
2020-11-09 16:02:25 -0300

    class

--- rpg_tracker/settings.py
+++ rpg_tracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=le0myp1xtmvra$&(x_g9wic2ch2@p+cx&%!xiurh1b^yhn8&!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
