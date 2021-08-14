b753b4d03a151fb675c93a4d2df6803f45e5ba0d
Lucas Westphal <westphallucas1@outlook.com>
2021-08-14 16:49:06 -0300

    folio universitas

--- rpg_tracker/accounts/views.py
+++ rpg_tracker/accounts/views.py
@@ -4,6 +4,6 @@ from rpg_tracker.core.models import FichaBase
 # Create your views here.
 def perfil(request):
     ctx = {}
-    if request.user.is_staff:
+    if request.user.is_superuser:
         ctx['fichas'] = FichaBase.objects.all()
     return render(request, 'perfil.html', ctx)
