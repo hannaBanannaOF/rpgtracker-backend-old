1a590264d926bd4f0adb6b3f9db31dcb24d95dae
Lucas Westphal <westphallucas1@outlook.com>
2022-01-16 22:47:39 -0300

    Filtros ADMIN

--- rpg_tracker/coc/admin.py
+++ rpg_tracker/coc/admin.py
@@ -2,4 +2,13 @@ from rpg_tracker.core.models import MesaBase
 from django.contrib import admin
 from .models import FichaCOC, Ocupation, Skills, SkillsOnFicha, Ammo, Weapons, WeaponsInFicha, MesaCOC, PulpTalents
 # Register your models here.
-admin.site.register([FichaCOC, Ocupation, Skills, SkillsOnFicha, Ammo, Weapons, WeaponsInFicha, MesaCOC, PulpTalents])
+
+class FichaCOCAdmin(admin.ModelAdmin):
+    list_filter = ('player',)
+
+class SkillOnFichaAdmin(admin.ModelAdmin):
+    list_filter = ('ficha',)
+
+admin.site.register(FichaCOC, FichaCOCAdmin)
+admin.site.register(SkillsOnFicha, SkillOnFichaAdmin)
+admin.site.register([Ocupation, Skills, SkillsOnFicha, Ammo, Weapons, WeaponsInFicha, MesaCOC, PulpTalents])
