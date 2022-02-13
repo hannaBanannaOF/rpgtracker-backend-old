10e40283e84789abcde47839c1655b96c8716e4c
Lucas Westphal <westphallucas1@outlook.com>
2022-02-12 22:27:19 -0300

    Ajuste

--- rpg_tracker/coc/serializers.py
+++ rpg_tracker/coc/serializers.py
@@ -9,7 +9,8 @@ class FichaCOCSerializer(serializers.ModelSerializer):
         model = FichaCOC
         fields =  ['age', 'birthplace', 'residence', 'sex', 'nome_personagem', 'jogador', 'pulp_cthulhu', 'pulp_archetype',
                     'strength', 'constitution', 'size', 'dexterity', 'appearence', 'inteligence', 'power', 'education', 'move_rate',
-                    'get_skill_list_as_array']
+                    'get_skill_list_as_array', 'hp', 'max_hp', 'major_wound', 'san', 'max_san', 'start_san', 'temporary_insanity', 
+                    'indefinity_insanity', 'luck', 'max_mp', 'mp', 'weapons', 'pulp_talents', 'dodge', 'build', 'bonus_dmg']
         depth = 1
 
 class OcupationSerizalizer(serializers.HyperlinkedModelSerializer):
