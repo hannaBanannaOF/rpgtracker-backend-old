0ffb97ec0e0460983831d3a678681b887153888a
Lucas Westphal <westphallucas1@outlook.com>
2022-04-25 10:13:29 -0300

    Improv view mestre

--- rpg_tracker/accounts/api_urls_v1.py
+++ rpg_tracker/accounts/api_urls_v1.py
@@ -5,4 +5,5 @@ app_name = 'api_accounts'
 urlpatterns = [
     path('current-user', views.current_user),
     path('current-user/fichas', views.MinhasFichasView.as_view()),
+    path('current-user/mesas-mestradas', views.MinhasMesasMestradasView.as_view()),
 ]
